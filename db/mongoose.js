9166ab22e8fcca4a3065ffc04c7b206b58296960
hosamhany <7ossHany@gmail.com>
2018-09-21 17:23:41 +0200

    minor edits

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-//TODO: change the localhost to the IP of the DB in the dockercompose file
 mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`, {})
 .then(success => console.log("Connected to Mongodb"))
 .catch(err => console.log(err));
